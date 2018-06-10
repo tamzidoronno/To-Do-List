19b6fdf370ae168da3ee02569897b8cd5fa2d0f2
Tamzid Rahman Oronno <tamzid.gregorian@gmail.com>
2018-06-10 16:26:54 +0600

    edit tasks

--- js/crud_action.js
+++ js/crud_action.js
@@ -1,8 +1,22 @@
 function action( varaibale ,GrandParent,parentID,child)
 {
+	var modal = document.getElementById('myModal');
+	var update = document.getElementById('update');
+	var cancel = document.getElementById('cancel');
+	var edittext = $(edittext).text();
+	
 	if (varaibale=="Edit")
 	{
-		alert('edit1') ;
+			
+			modal.style.display = "block";
+
+    		update.onclick = function() {
+		    GrandParent.text(edittext); //edited text gets updated here
+		    modal.style.display = "none";
+			}
+    		cancel.onclick = function() {
+		    modal.style.display = "none";
+			}
 	}
 	else if (varaibale=="Delete")
 	{
